9a059dc7c20eb48bb2de044afcf48682148eae22
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-03-22 15:55:02 +0200

    Excercise: 4.6*: apufunktioita ja yksikkötestejä, step4

--- blogilista/tests/list_helper.test.js
+++ blogilista/tests/list_helper.test.js
@@ -107,3 +107,53 @@ describe('favorite blog', () => {
     assert.deepStrictEqual(result, blogs[2]);
   });
 });
+
+describe('most blogs', () => {
+  const blogs = [
+    {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    },
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    },
+    {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      likes: 10
+    },
+    {
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      likes: 0
+    },
+    {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      likes: 2
+    }
+  ];
+
+  test('of empty list is null', () => {
+    const result = listHelper.mostBlogs([]);
+    assert.strictEqual(result, null);
+  });
+
+  test('returns the author with the highest blog count', () => {
+    const result = listHelper.mostBlogs(blogs);
+    const expected = {
+      author: 'Robert C. Martin',
+      blogs: 3
+    };
+
+    assert.deepStrictEqual(result, expected);
+  });
+});
--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -17,8 +17,31 @@ const favoriteBlog = (blogs) => {
   });
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0)
+  {
+    return null;
+  }
+
+  const counts = {};
+
+  blogs.forEach((blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1;
+  });
+
+  const topAuthor = Object.keys(counts).reduce((max, author) => {
+    return counts[author] > counts[max] ? author : max;
+  });
+
+  return {
+    author: topAuthor,
+    blogs: counts[topAuthor]
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 };
