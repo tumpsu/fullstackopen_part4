f68ff3f74140a79cbd28ba61f7fd65734a51cfae
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-03-24 18:49:05 +0200

    Excercise: 4.18: blogilistan laajennus, step6

--- blogilista/app.js
+++ blogilista/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
+const loginRouter = require('./controllers/login');
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +12,10 @@ app.use(middleware.requestLogger);
 
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
+
 
 module.exports = app;
